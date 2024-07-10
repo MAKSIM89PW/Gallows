ea06f70819bfb4a6ef0cccf8265192c247451c65
Me <rubikone89pw@rambler.ru>
2024-07-11 01:42:20 +0300

    add print gallows

--- src/main/java/StartGame.java
+++ src/main/java/StartGame.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class StartGame {
     static int errorCount;                 // Учет ошибок
+    static List<Character> errorChar;      // Учет неверно указанных букв
     static FieldMap fieldmap;              // Текущий вид виселицы
     static String hiddenWord;              // Загаданное слово
     static String displayingTheHiddenWord; // Отображение загаданного слово
@@ -10,8 +13,14 @@ public class StartGame {
     public static void startGame(){
         String[] field = { "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX"};
         errorCount = 0;
+        errorChar = new ArrayList<>();
         hiddenWord = RandomWords.getWords().toUpperCase(Locale.ROOT);
         fieldmap = FieldMap.valueOf(field[errorCount]);
         displayingTheHiddenWord = "*".repeat(hiddenWord.length());
+        boolean vinner = true;
+
+        while(vinner) {
+            System.out.println(fieldmap);
+        }
     }
 }
