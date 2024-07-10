48f108f514477ba5a8d33b028577e16b82041dff
Me <rubikone89pw@rambler.ru>
2024-07-11 01:00:50 +0300

    The initial condition of the gallows

---
+++ src/main/java/StartGame.java
@@ -0,0 +1,12 @@
+public class StartGame {
+    static int errorCount;
+    static FieldMap fieldmap;
+
+
+    public static void startGame{
+        String[] field = { "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX"};
+        errorCount = 0;
+        fieldmap = FieldMap.valueOf(field[errorCount]);
+
+    }
+}
