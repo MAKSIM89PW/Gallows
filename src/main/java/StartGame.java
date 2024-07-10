00936b6499f130d900d911d630945738876e6ff1
Me <rubikone89pw@rambler.ru>
2024-07-11 02:32:04 +0300

    creating letter input logic

--- src/main/java/StartGame.java
+++ src/main/java/StartGame.java
@@ -8,7 +8,7 @@ public class StartGame {
     static FieldMap fieldmap;              // Текущий вид виселицы
     static String hiddenWord;              // Загаданное слово
     static String displayingTheHiddenWord; // Отображение загаданного слово
-
+    static final int errorCountGame = 6;   // Допустимые ошибки
 
     public static void startGame(){
         String[] field = { "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX"};
@@ -21,12 +21,43 @@ public class StartGame {
 
         while(vinner) {
             System.out.println(fieldmap);
-            System.out.println("слово" + displayingTheHiddenWord);
+            System.out.println("слово:  " + displayingTheHiddenWord);
             System.out.println("ошибка (" + errorCount + "): " + errorChar);
             System.out.println("введите букву: ");
             Character input = Main.scanner.nextLine().toUpperCase(Locale.ROOT).charAt(0);
             System.out.println("\n \n \n \n \n \n \n \n \n \n");
 
+            if (hiddenWord.contains(String.valueOf(input))) {
+                char[] temp = hiddenWord.toCharArray();
+                char[] tempCurrent = displayingTheHiddenWord.toCharArray();
+
+                for(int i = 0; i < displayingTheHiddenWord.length(); i++) {
+                    if(temp[i] == input) {
+                        tempCurrent[i] = input;
+                    }
+                }
+
+                displayingTheHiddenWord = new String(tempCurrent);
+                if(!displayingTheHiddenWord.contains("*")) {
+                    vinner = false;
+                }
+            } else {
+                if(errorChar.contains(input)) {
+                    System.out.println("буква повторяеться");
+
+                } else {
+                    errorChar.add(input);
+                }
+                errorCount++;
+                fieldmap = FieldMap.valueOf(field[errorCount]);
+                if(errorCount == errorCountGame) vinner = false;
+            }
+        }
+        if(errorCount != errorCountGame) {
+            System.out.println("\n \n \tВы выйграли \n \n");
+        } else {
+            System.out.println(FieldMap.SIX);
+            System.out.println("\n \tМного ошибок - проигрыш \n \n ");
         }
     }
 }
