725536892e3fdd02b985658649faefc94f71ee3d
Me <rubikone89pw@rambler.ru>
2024-07-11 01:15:23 +0300

    getting and displaying a word

--- src/main/java/StartGame.java
+++ src/main/java/StartGame.java
@@ -1,12 +1,16 @@
-public class StartGame {
-    static int errorCount;
-    static FieldMap fieldmap;
+import java.util.Locale;
 
+public class StartGame {
+    static int errorCount;                 // Учет ошибок
+    static FieldMap fieldmap;              // Текущий вид виселицы
+    static String hiddenWord;              // Загаданное слово
+    static String displayingTheHiddenWord; // Отображение загаданного слово
 
     public static void startGame{
         String[] field = { "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX"};
         errorCount = 0;
+        hiddenWord = RandomWords.getWords().toUpperCase(Locale.ROOT);
         fieldmap = FieldMap.valueOf(field[errorCount]);
-
+        displayingTheHiddenWord = "*".repeat(hiddenWord.length());
     }
 }
